52e805b7ce80293999276654203d49431ff473c9
JuanGarciaReyes <45839913+JuanGarciaReyes@users.noreply.github.com>
2020-02-02 02:29:13 -0700

    Add files via upload

---
+++ Bishop.java
@@ -0,0 +1,36 @@
+import java.lang.Math;
+
+public class Bishop {
+  boolean color;
+  char x_move;
+  int y_move;
+
+  public void set_color(boolean color){
+    this.color = color;
+  }
+  public void setpos_x(char x_move) {
+    this.x_move = x_move;
+  }
+  public void setpos_y(int y_move) {
+    this.y_move = y_move;
+  }
+  public char getpos_x() {
+    return this.x_move;
+  }
+  public int getpos_y() {
+    return this.y_move;
+  }
+  public boolean get_color(){
+    return this.color;
+  }
+  public static boolean check_movement( int x, int y, int pos_x, int pos_y){
+    if(Math.abs(pos_x-x)==Math.abs(pos_y-y)){
+      System.out.println("a valid move");
+      return true;
+    }
+    else{
+      System.out.println("an invalid move");
+    return false;
+   }
+  }// end of method
+ }//end of class
