dfec5afd1f0224a00964db69db61075a690a9176
JuanGarciaReyes <45839913+JuanGarciaReyes@users.noreply.github.com>
2020-02-02 03:20:53 -0700

    Update Chess.java

--- chess/Chess.java
+++ chess/Chess.java
@@ -1,3 +1,9 @@
+/*
+Juan Angel Garcia Reyes.
+80591934.
+T-TH 3:00- 4:20.
+Professor- Dr. Oscar Mondragon.
+*/
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -15,10 +21,19 @@ public class Chess{
     Pawn pawn = new Pawn(); Rook rook = new Rook(); Bishop bishop = new Bishop(); Knight knight = new Knight(); Queen queen = new Queen(); King king = new King(); //calling and creating pieces objects
     System.out.println("Which position in X do you want to start?");
     char first_x = scanner.next().charAt(0); //input the position in x
+    if((first_x != 'a')&&(first_x !='A')&&(first_x !='b')&&(first_x !='B')&&(first_x !='c')&&(first_x !='C')&&(first_x !='d')&&(first_x !='D')&&(first_x !='e')&&(first_x !='E')&&(first_x !='f')&&(first_x !='F')
+    &&(first_x !='g')&&(first_x !='G')&&(first_x !='h')&&(first_x !='H')){
+      System.out.println("try again later please");
+      return;
+    }
     pawn.setpos_x(first_x); rook.setpos_x(first_x); knight.setpos_x(first_x); bishop.setpos_x(first_x); queen.setpos_x(first_x); king.setpos_x(first_x); //setters for x position
     System.out.println("Which position in Y do you want to start?");
     String first_y = scanner.next();// input the position in y
     int first_convertion = Integer.parseInt(first_y); //convertion of the input entry
+    if(first_convertion>8){
+      System.out.println("try again later please");
+      return;
+    }
     pawn.setpos_y(first_convertion); rook.setpos_y(first_convertion); knight.setpos_y(first_convertion); bishop.setpos_y(first_convertion); queen.setpos_y(first_convertion); king.setpos_y(first_convertion); //setters for x position
     System.out.println("which color would you like ( 0 for white, 1 for black) ?");
     int color = scanner.nextInt();
@@ -42,6 +57,9 @@ System.out.println();
     System.out.println("Which position in Y do you want to move?");
     String answer_y = scanner.next();// input the position in y
     int answer_convertion = Integer.parseInt(answer_y); //convertion of the input entry
+    System.out.println("----------------------------------------");
+    System.out.println("Your position is " + answer_x+answer_y);
+    System.out.println("----------------------------------------");
     HashMap<Character, Integer> converter = new HashMap<Character, Integer>(); // Hashmmap creation for changing letters to numbers
     converter.put('A', 1); converter.put('B', 2); converter.put('C', 3); converter.put('D', 4);converter.put('E', 5);converter.put('F', 6);converter.put('G', 7);converter.put('H', 8);
     converter.put('a', 1); converter.put('b', 2); converter.put('c', 3); converter.put('d', 4);converter.put('e', 5);converter.put('f', 6);converter.put('h', 7);converter.put('h', 8);
